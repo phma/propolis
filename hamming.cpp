0f360eae0ec06cfd46233d097b855f6163703fd9
Pierre Abbat <phma@bezitopo.org>
2017-04-10 05:40:05 +0000

    start coding Hamming code (untested)

--- hamming.cpp
+++ hamming.cpp
@@ -3,6 +3,8 @@
 /* hamming.cpp - Hamming codes                        */
 /*                                                    */
 /******************************************************/
+#include "hamming.h"
+using namespace std;
 
 /* Hamming codes, as used in Propolis, are of length at least 3. Each of
  * the five layers consists of Hamming code blocks; all five layers have
@@ -16,3 +18,25 @@
  * 29694: 29694 (29679); ... 7425,(4)3712,(3)3711 (29597); (21)127,(429)63 (26973);
  *   ... (4242)7 (16968); (9898)3 (9898)
  */
+
+vector<signed char> Hamming::getCode()
+{
+  return code;
+}
+
+void Hamming::setCode(vector<signed char> Code)
+{
+  code=Code;
+}
+
+void Hamming::push_back(char byte)
+// Appends a data byte. Check bytes are added as needed.
+{
+  int i;
+  while ((code.size()&(code.size()+1))==0)
+    code.push_back(0);
+  code.push_back(byte);
+  for (i=1;i<=code.size();i<<=1)
+    if (i&code.size())
+      code[i-1]^=byte;
+}
--- hamming.h
+++ hamming.h
@@ -4,3 +4,18 @@
 /*                                                    */
 /******************************************************/
 
+#include <vector>
+
+class Hamming
+{
+protected:
+  std::vector<signed char> code;
+  /* signed char is used for belief propagation, where the soft bit ranges
+   * from -127 for 1 to 127 for 0. -127 means 1 so that * can mean xor.
+   * code can also contain hard bits, normally one per byte.
+   */
+public:
+  std::vector<signed char> getCode();
+  void setCode(std::vector<signed char> Code);
+  void push_back(char byte);
+};
