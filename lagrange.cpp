78e0d07b1b3a42912de1b08a4638babebdd94013
Pierre Abbat <phma@bezitopo.org>
2018-05-16 06:16:07 +0000

    Define poly31 class
    It's a polynomial of numbers mod 31.

--- lagrange.cpp
+++ lagrange.cpp
@@ -11,6 +11,11 @@ const char inv31[]=
    5, 6,23,10,15,30
 };
 
+int31::int31()
+{
+  n=0;
+}
+
 int31::int31(int m)
 {
   n=m%31;
@@ -42,4 +47,124 @@ int31 int31::operator/(int31 b)
 {
   assert(b.n);
   return int31(n*inv31[b.n]);
+}
+
+int31& int31::operator+=(int31 b)
+{
+  n=(n+b.n)%31;
+  return *this;
+}
+
+int31& int31::operator-=(int31 b)
+{
+  n=(n-b.n+31)%31;
+  return *this;
+}
+
+int31& int31::operator*=(int31 b)
+{
+  n=(n*b.n)%31;
+  return *this;
+}
+
+poly31::poly31()
+{
+  int i;
+  for (i=0;i<30;i++)
+    coeff[i]=0;
+}
+
+int31 poly31::operator()(int31 x)
+{
+  int i;
+  int31 acc,pow;
+  for (acc=i=0,pow=1;i<30;i++)
+  {
+    acc+=pow*coeff[i];
+    pow*=x;
+  }
+  return acc;
+}
+
+poly31& poly31::operator=(int31 b)
+{
+  int i;
+  coeff[0]=b;
+  for (i=1;i<30;i++)
+    coeff[i]=0;
+}
+
+poly31 poly31::operator<<(int n)
+{
+  int i;
+  poly31 ret;
+  n%=30;
+  if (n<0)
+    n+=30;
+  for (i=0;i<30;i++)
+    ret.coeff[(i+n)%30]=coeff[i];
+  return ret;
+}
+
+poly31 poly31::operator>>(int n)
+{
+  int i;
+  poly31 ret;
+  n%=30;
+  if (n<0)
+    n+=30;
+  for (i=0;i<30;i++)
+    ret.coeff[i]=coeff[(i+n)%30];
+  return ret;
+}
+
+poly31 poly31::operator+(int31 b)
+{
+  poly31 ret=*this;
+  ret.coeff[0]+=b;
+  return ret;
+}
+
+poly31 poly31::operator+(poly31 &b)
+{
+  poly31 ret;
+  int i;
+  for (i=0;i<30;i++)
+    ret.coeff[i]=coeff[i]+b.coeff[i];
+  return ret;
+}
+
+poly31 poly31::operator-(int31 b)
+{
+  poly31 ret=*this;
+  ret.coeff[0]-=b;
+  return ret;
+}
+
+poly31 poly31::operator-(poly31 &b)
+{
+  poly31 ret;
+  int i;
+  for (i=0;i<30;i++)
+    ret.coeff[i]=coeff[i]-b.coeff[i];
+  return ret;
+}
+
+poly31 poly31::operator*(int31 b)
+{
+  poly31 ret;
+  int i;
+  for (i=0;i<30;i++)
+    ret.coeff[i]=coeff[i]*b;
+  return ret;
+}
+
+poly31 poly31::operator*(poly31 &b)
+{
+  poly31 ret;
+  int i,j;
+  for (i=0;i<30;i++)
+    for (j=0;j<30;j++)
+      ret.coeff[(i+j)%30]+=coeff[i]*b.coeff[j];
+  return ret;
 }
--- lagrange.h
+++ lagrange.h
@@ -1,17 +1,37 @@
 /* Lagrange polynomial interpolation in F31
  */
 
-#include <vector>
-
 class int31
 {
 private:
   int n;
 public:
+  int31();
   int31(int m);
   int val();
   int31 operator+(int31 b);
   int31 operator-(int31 b);
   int31 operator*(int31 b);
   int31 operator/(int31 b);
+  int31& operator+=(int31 b);
+  int31& operator-=(int31 b);
+  int31& operator*=(int31 b);
+};
+
+class poly31
+{
+private:
+  int31 coeff[30]; // By Fermat's little theorem
+public:
+  poly31();
+  int31 operator()(int31 x);
+  poly31& operator=(int31 b);
+  poly31 operator<<(int n);
+  poly31 operator>>(int n);
+  poly31 operator+(int31 b);
+  poly31 operator+(poly31 &b);
+  poly31 operator-(int31 b);
+  poly31 operator-(poly31 &b);
+  poly31 operator*(int31 b);
+  poly31 operator*(poly31 &b);
 };
