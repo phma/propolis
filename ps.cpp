308f423f77140c0266e62aa8815e8e077a5b7a7a
Pierre Abbat <phma@bezitopo.org>
2021-07-02 09:53:08 +0000

    Add the rest of the file-top banners

--- ps.cpp
+++ ps.cpp
@@ -3,6 +3,23 @@
 /* ps.cpp - PostScript output                         */
 /*                                                    */
 /******************************************************/
+/* Copyright 2012-2017 Pierre Abbat.
+ * This file is part of Propolis.
+ * 
+ * The Propolis program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Propolis is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License and Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Propolis. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 /* The PostScript file is 35.7 times the information contained in it;
  * when gzipped, it is 5.85 times as big as the information.
